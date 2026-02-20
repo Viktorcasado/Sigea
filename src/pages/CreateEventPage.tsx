3fc80892e5fe907181d9fbf6433a0d9b30a542fa
[dyad] <viktorcasado@gmail.com>
2026-02-20 01:25:30 -0300

    [dyad] Correção de erro de renderização (tela branca) - wrote 2 file(s)

--- src/pages/CreateEventPage.tsx
+++ src/pages/CreateEventPage.tsx
@@ -127,7 +127,7 @@ export default function CreateEventPage() {
                 <input 
                   type="number" 
                   value={cargaHoraria} 
-                  onChange={(e) => setCariaHoraria(Number(e.target.value))} 
+                  onChange={(e) => setCargaHoraria(Number(e.target.value))} 
                   className="w-full pl-10 pr-4 py-3 bg-gray-50 border border-gray-200 rounded-xl focus:ring-2 focus:ring-indigo-500 focus:bg-white outline-none transition-all"
                 />
                 <Users className="absolute left-3 top-3.5 text-gray-400 w-5 h-5" />
